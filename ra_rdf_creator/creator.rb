59d33e94894471c12abddb0a2ea8bcaa22b56255
Hiroyuki Inoue <mammymax@gmail.com>
2012-08-09 18:00:14 +0900

    add rdf_xml_dump method

--- ra_rdf_creator/creator.rb
+++ ra_rdf_creator/creator.rb
@@ -1,10 +1,9 @@
 require 'sequel'
+require 'rdf'
+require 'rdf/rdfxml'
 require './../../research/olap_etl/util.rb'
 
-def main
-  db = Util.connect_db( { :db => 'ra' } )
-  tables = [ 'ev_time', 'observation_instance' ]
-
+def csv_dump( db, tables )
   tables.each do |table|
     table_name = table.to_sym
     file = File.open( "#{table.to_s}.csv", "w" )
@@ -14,6 +13,40 @@ def main
     file.close
     puts "#{table.to_s}.csv was closed"
   end
+end
+
+require 'pp'
+require 'rdf/ntriples'
+
+def rdf_xml_dump( db, tables )
+  tables.each do |table|
+    table_name = table.to_sym
+#    file = File.open( "#{table.to_s}.rdf", "w" )
+#    RDF::RDFXML::Writer.open( "#{table.to_s}.rdf" ) do |writer|
+    RDF::Writer.for( :ntriples ).open( "#{table.to_s}.rdf" ) do |writer|
+      db[table_name].all.each do |r|
+        stm = RDF::Statement.new
+        stm.subject = r[:subject]
+        stm.predicate = r[:predicate]
+        stm.object = r[:object]
+
+        if r[:value_type_id] == "2" && r[:value_type] != nil
+          l = RDF::Literal.new( r[:object], :datatype => r[:value_type] )
+          stm.object = l
+        end
+        writer << stm
+      end
+    end
+    puts "#{table.to_s}.rdf was closed"
+  end
+end
+
+def main
+  db = Util.connect_db( { :db => 'ra' } )
+  tables = [ 'ev_time', 'observation_instance' ]
+
+#  csv_dump( db, tables )
+  rdf_xml_dump( db, tables )
 end
 
 main
