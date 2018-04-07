384a2aae766cd1abfe11d4db38e6f2f10e80eb40
Hiroyuki Inoue <mammymax@gmail.com>
2018-04-07 16:01:13 -0700

    Add application#hello

--- rails_5x/hello_world/app/controllers/application_controller.rb
+++ rails_5x/hello_world/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "Hello, World!"
+  end
+
+  def goodbye
+    render html: "さようなら、世界！"
+  end
 end
